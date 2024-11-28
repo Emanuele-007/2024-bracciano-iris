9ca88d516da4730673eae1d8fb1543b96c31aff3
Your Name <vian@libero.com>
2024-11-28 16:10:47 +0100

    add petal plot

--- plot_iris.py
+++ plot_iris.py
@@ -5,13 +5,15 @@ import matplotlib.pyplot as plt
 
 # Caricamento del dataset
 iris = load_iris()
+print (iris)
 X = iris.data
 y = iris.target
 
 # Visualizzazione della distribuzione delle classi
+#la figura in plt è la finestra
 plt.figure(figsize=(12, 4))
 
-# Plot per i sepali
+#Plot per i sepali
 plt.subplot(1, 2, 1)
 for i in range(3):
     mask = y == i
@@ -22,6 +24,14 @@ plt.title('Distribuzione dei Sepali')
 plt.legend()
 
 # Plot per i petali
+plt.subplot(1, 2, 2)
+for i in range(3):
+    mask = y == i
+    plt.scatter(X[mask, 2], X[mask, 3], label=iris.target_names[i])
+plt.xlabel('Lunghezza petalo')
+plt.ylabel('Larghezza petalo')
+plt.title('Distribuzione dei petali')
+plt.legend()
 
 # Ora tocca a te! Prova a fare il plot dei petali!
 # Poi fai commit e push
